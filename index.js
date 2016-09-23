6273aba8bbb83a5d1c2d75789f211c5209cf0fed
Jeff Micklos <jeffmicklos@gmail.com>
2016-09-23 15:52:22 -0700

    first step to preso

--- index.js
+++ index.js
@@ -13,6 +13,10 @@ app.get('/', function(request, response) {
   response.render('pages/index');
 });
 
+app.get('/webpack', function(request, response) {
+  response.render('pages/webpack');
+});
+
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
